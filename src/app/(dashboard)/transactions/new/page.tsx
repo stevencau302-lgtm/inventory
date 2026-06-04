e0d9644c5ef27a82c8599a2345961d8f8584ee5b
ersetdigital-sudo <ersetdigital@gmail.com>
2026-06-04 20:30:00 +0700

    auto: update files

--- src/app/(dashboard)/transactions/new/page.tsx
+++ src/app/(dashboard)/transactions/new/page.tsx
@@ -261,9 +261,9 @@ export default function NewTransactionPage() {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row gap-6">
-      {/* LEFT - Form */}
-      <div className="flex-1 min-w-0">
+      <div className="flex flex-col gap-6">
+      {/* Form */}
+      <div className="max-w-3xl w-full">
       <form id="txForm" onSubmit={handleSubmit} className="space-y-8">
 
         {/* === TIPE TRANSAKSI === */}
